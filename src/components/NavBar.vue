737366be3e0728511b2762e48114258ab6738a79
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2018-07-09 18:08:55 +0200

    Add navigation bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-navigation-drawer
+    persistent
+    :mini-variant="miniVariant"
+    :clipped="clipped"
+    v-model="drawer"
+    enable-resize-watcher
+  >
+    <v-list>
+      <v-list-tile
+        value="true"
+        v-for="year in years"
+        :key="year"
+        router
+        :to="'/'"
+      >
+        <!--<v-list-tile-action>
+          <v-icon v-html="year.icon"></v-icon>
+        </v-list-tile-action>-->
+        <v-list-tile-content>
+          <v-list-tile-title v-text="year"></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: [
+    'years',
+    'miniVariant',
+    'clipped',
+    'drawer',
+  ],
+};
+</script>
